d11f479602e594d55cf82fb6647f864c7b32e46b
EliasCarrilloD <eliasgabrielcd@gmail.com>
2024-12-22 11:45:56 -0600

    Revert "Merge branch 'main' of https://github.com/PLINIORZAVALA/frontendIOSA"
    This reverts commit 8969c1ecb9ccf4ff95996f0900662b5a75def7be, reversing
    changes made to b0688cebb965863a5cb54290c89ea5e3208b8589.

--- src/app/components/internauta/services/service.component.ts
+++ src/app/components/internauta/services/service.component.ts
@@ -1,41 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule si es standalone
-import { ServiceService } from '../../../service/service.service'; // Ruta al servicio
+import { CommonModule } from '@angular/common';  // Asegúrate de importar CommonModule si es un componente standalone
+import { ServiceService } from '../../../service/service.service';  // Ruta al servicio
 import { Service } from '../../../interfaces/services/service.interface';
 
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
   styleUrls: ['./service.component.css'],
-  imports: [CommonModule], // Si el componente es standalone
+  imports: [CommonModule]  // Si el componente es standalone, asegúrate de importar CommonModule
 })
 export class ServiceComponent implements OnInit {
-  services: Service[] = []; // Lista completa de servicios
-  errorMessage: string = ''; // Variable para manejar errores
+  services: Service[] = [];  // Variable para almacenar los servicios
+  errorMessage: string = ''; // Variable para manejar los errores
 
   constructor(private serviceService: ServiceService) {}
 
   ngOnInit(): void {
-    this.loadServices(); // Cargar los servicios al inicializar el componente
+    this.loadServices();  // Llamamos a la función para cargar los servicios cuando el componente se inicializa
   }
 
   loadServices(): void {
     this.serviceService.getServices().subscribe({
       next: (data) => {
-        // Asignar datos y generar la ruta de las imágenes
-        this.services = data.map((service) => ({
-          ...service,
-          img: this.getImagePath(service.img), // Generar ruta completa para la imagen
-        }));
+        this.services = data;  // Asigna los datos de servicios a la variable
       },
       error: (err) => {
-        this.errorMessage = err.message; // Manejar errores
-      },
+        this.errorMessage = err.message;  // Maneja los errores y muestra el mensaje
+      }
     });
-  }
-
-  getImagePath(nombreImagen: string): string {
-    // Generar ruta para las imágenes
-    return `home/company/${nombreImagen}`;
   }
 }
--- src/app/interfaces/services/service.interface.ts
+++ src/app/interfaces/services/service.interface.ts
@@ -2,22 +2,19 @@
 export interface Service {
     idService: number;
     tipo: string;
-    img: string;
     descripcion: string;
-    companyId: number; 
+    companyId: number; // Se puede usar para asociar un servicio con una empresa
   }
   
   // Interface para la creación de un servicio
   export interface CreateService {
     tipo: string;
     descripcion: string;
-    img: string;
   }
   
   // Interface para actualizar un servicio
   export interface UpdateService {
     idService: number;
-    img?: string;
     tipo?: string;
     descripcion?: string;
   }
